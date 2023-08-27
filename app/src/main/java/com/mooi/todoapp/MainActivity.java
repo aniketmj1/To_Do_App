bc288948db849ad5d8278d0584374bdfbbe0f87d
Amit <aniketmj.kumar@gmail.com>
2023-08-27 18:07:33 +0530

    initial commit

---
+++ app/src/main/java/com/mooi/todoapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.mooi.todoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
